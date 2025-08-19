1a4462140cd55b370ae6496f4feec1889bec1424
rohan <Rohan.Thapamagar@metropolia.fi>
2025-08-19 14:36:34 +0300

    initial commit

---
+++ MOD 01/01 Eka.py
@@ -0,0 +1,14 @@
+print('Terve luokka')
+print('Hei, Rohan!"')
+print('Moi, "Bossman"')
+print("moi\nrohan")
+print(8)
+
+
+# Yksinkertainen muuttuja
+nimi = ('Ruoho')
+print(nimi)
+
+# syöte, eli tietoa käyttäjältä
+nimi2 = input('Moikka, anna nimi:')
+print(nimi2)
