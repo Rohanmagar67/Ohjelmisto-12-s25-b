79065602904eea89a561b4ebbf31aff2eb9e97af
rohan <Rohan.Thapamagar@metropolia.fi>
2025-09-16 21:50:41 +0300

    Tehty MOD 08 T1-2

---
+++ MOD 08 Relaatiotietokannan käyttö/T1.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+def hae_lentokentta_icao_koodilla(icao):
+    sql = f"SELECT name, municipality FROM airport WHERE ident = '{icao}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    kursori.close()
+    return tulos
+
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='rohan',
+         password='#Ruoho69',
+         autocommit=True
+         )
+
+icao = input("Anna lentokentän ICAO-koodi: ")
+lentokentta = hae_lentokentta_icao_koodilla(icao)
+
+if lentokentta:
+    nimi, kunta = lentokentta
+    print(f"Lentokentän nimi: {nimi}")
+    print(f"Kunta nimi: {kunta}")
+
+yhteys.close()
---
+++ MOD 08 Relaatiotietokannan käyttö/T2.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+def hae_maa_koodilla(maa_koodi):
+    sql = f"""
+    select type, count(*) as maara
+    from airport 
+    where iso_country = '{maa_koodi}'
+    group by type
+    order by maara
+    """
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulokset = kursori.fetchall()
+    kursori.close()
+    return tulokset
+    
+def hae_maa_nimi(koodi):
+    sql = f"select name from country where iso_country = '{koodi}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    kursori.close()
+    if tulos:
+        return tulos[0]
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='rohan',
+         password='#Ruoho69',
+         autocommit=True
+         )
+    
+maa_koodi = input("Anna maakoodi: ")
+maan_nimi = hae_maa_nimi(maa_koodi)
+lentokentat = hae_maa_koodilla(maa_koodi)
+    
+print(f"{maan_nimi} {maa_koodi}")
+    
+if lentokentat:
+    for tyyppi, maara in lentokentat:
+        print(f"{tyyppi}: {maara} kappaletta")
